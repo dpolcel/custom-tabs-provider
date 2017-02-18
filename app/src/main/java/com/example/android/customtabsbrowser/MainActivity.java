e9619c6a0da62a667c65455590cfca1711e187c5
polcel <diegot@outlook.com>
2017-02-18 12:48:12 -0200

    First commit. Implementation of chrome custom tabs.

--- app/src/main/java/com/example/android/customtabsbrowser/MainActivity.java
+++ app/src/main/java/com/example/android/customtabsbrowser/MainActivity.java
@@ -15,15 +15,22 @@
  */
 package com.example.android.customtabsbrowser;
 
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -47,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mUrlTextView;
     private TextView mTitleView;
     private CustomTabController mCustomTabController;
+    private LockscreenUtils mLockscreenUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         mTitleView = (TextView) findViewById(R.id.titleView);
 
         mBrowser = (WebView) findViewById(R.id.webView);
+        clearCookies(getApplicationContext());
         mBrowser.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onReceivedTitle(WebView view, String title) {
@@ -74,6 +83,28 @@ public class MainActivity extends AppCompatActivity {
         });
         mBrowser.setWebViewClient(new WebViewClient());
         mBrowser.getSettings().setJavaScriptEnabled(true);
+        mBrowser.getSettings().setDomStorageEnabled(true);
+
+        //toggleHideyBar();
+        getWindow().getDecorView().setSystemUiVisibility(8);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void clearCookies(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            Log.d("COOKIE_CLEARING", "Using clearCookies code for API >=" + String.valueOf(Build.VERSION_CODES.LOLLIPOP_MR1));
+            CookieManager.getInstance().removeAllCookies(null);
+            CookieManager.getInstance().flush();
+        } else {
+            Log.d("COOKIE_CLEARING", "Using clearCookies code for API <" + String.valueOf(Build.VERSION_CODES.LOLLIPOP_MR1));
+            CookieSyncManager cookieSyncMngr = CookieSyncManager.createInstance(context);
+            cookieSyncMngr.startSync();
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.removeAllCookie();
+            cookieManager.removeSessionCookie();
+            cookieSyncMngr.stopSync();
+            cookieSyncMngr.sync();
+        }
     }
 
     @Override
@@ -85,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.error_no_custom_tab, Toast.LENGTH_LONG).show();
         }
     }
+
 
     @Override
     public void finish() {
@@ -104,11 +136,55 @@ public class MainActivity extends AppCompatActivity {
         if (mCustomTabController.onOptionsItemSelected(item)) {
             return true;
         }
-        if (R.id.action_open_in_browser == item.getItemId()) {
-            startActivity(new Intent(Intent.ACTION_VIEW, getIntent().getData()));
-            return true;
-        }
+//        if (R.id.action_open_in_browser == item.getItemId()) {
+//            startActivity(new Intent(Intent.ACTION_VIEW, getIntent().getData()));
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleHideyBar() {
+
+        // BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+            Log.i(TAG, "Turning immersive mode mode off. ");
+        } else {
+            Log.i(TAG, "Turning immersive mode mode on.");
+        }
+
+        newUiOptions ^= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+        //END_INCLUDE (set_ui_flags)
     }
 
     /**
